842779dd76fd926514e910a3efa007aa5eb571e5
sinsokun <sinsokun13@gamil.com>
2023-11-06 19:07:35 +0700

    First commit repository

---
+++ src/main/java/com/istad/gitbas/GitbasApplication.java
@@ -0,0 +1,13 @@
+package com.istad.gitbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitbasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/istad/gitbas/GitbasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istad.gitbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitbasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
